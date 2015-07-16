fdccbc652de36ef7e733583f801e1dd76ee9f121
Tiago Madeira <tmadeira@gmail.com>
2015-07-15 20:47:31 -0700

    Reorganize API template code

--- src/php/api.php
+++ src/php/api.php
@@ -6,21 +6,23 @@ Header('Content-Type: application/json; charset=utf-8');
 
 require_once(__DIR__ . '/dlib/api/routes.php');
 
-try {
-  if (!array_key_exists('route', $_REQUEST)) {
-    Header('HTTP/1.0 404');
-    echo json_encode(Array('error' => 'Request missing route'));
-    exit();
-  }
-
+if (array_key_exists('route', $_REQUEST)) {
   $route = $_REQUEST['route'];
-  if (!array_key_exists($route, $DOLORES_ROUTES)) {
-    Header('HTTP/1.0 404');
-    echo json_encode(Array('error' => 'Invalid route'));
-    exit();
-  }
+} else {
+  Header('HTTP/1.0 404');
+  echo json_encode(Array('error' => 'Request missing route'));
+  exit();
+}
 
+if (array_key_exists($route, $DOLORES_ROUTES)) {
   $endpoint = $DOLORES_ROUTES[$route];
+} else {
+  Header('HTTP/1.0 404');
+  echo json_encode(Array('error' => 'Invalid route'));
+  exit();
+}
+
+try {
   $API = new $endpoint($_SERVER['REQUEST_METHOD']);
   echo $API->process();
 } catch (Exception $e) {
