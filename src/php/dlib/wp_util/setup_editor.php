b293aded8adad0722a787d94db79a7e77ce69957
Tiago Madeira <tmadeira@gmail.com>
2015-07-19 20:49:19 -0700

    Style WP editor

---
+++ src/php/dlib/wp_util/setup_editor.php
@@ -0,0 +1,2 @@
+<?php
+add_editor_style(get_template_directory_uri() . '/editor.css');
--- src/php/functions.php
+++ src/php/functions.php
@@ -2,4 +2,5 @@
 require_once(__DIR__ . '/dlib/wp_util/disable_admin_bar.php');
 require_once(__DIR__ . '/dlib/wp_util/disable_emojis.php');
 require_once(__DIR__ . '/dlib/wp_util/register_menus.php');
+require_once(__DIR__ . '/dlib/wp_util/setup_editor.php');
 require_once(__DIR__ . '/dlib/wp_util/setup_thumbnails.php');
