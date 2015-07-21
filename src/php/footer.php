3976855142e816e19d90fa1485ef28e743e1dcc6
Tiago Madeira <tmadeira@gmail.com>
2015-07-20 22:46:18 -0700

    Add social buttons (Facebook and email)

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,32 +1,43 @@
 <?php
 require_once(__DIR__ . '/dlib/assets.php');
+
+$icn_facebook = DoloresAssets::get_image_uri('social-facebook.svg');
+$icn_email = DoloresAssets::get_image_uri('social-email.svg');
+
+$img_flc = DoloresAssets::get_image_uri('logo-flc.png');
+$img_psol = DoloresAssets::get_image_uri('logo-psol-carioca.png');
 ?>
 <footer class="site-footer">
   <div class="wrap">
     <ul class="footer-list">
-      <!-- TODO: add social icons -->
-    </ul>
-    <ul class="footer-list footer-supporters">
-      <?php
-      $img_flc = DoloresAssets::get_image_uri('logo-flc.png');
-      $img_psol = DoloresAssets::get_image_uri('logo-psol-carioca.png');
-      ?>
       <li>
-        <a href="http://laurocampos.org.br/" title="Fundação Lauro Campos">
-          <img
-            alt="Fundação Lauro Campos"
-            class="footer-supporter-banner"
-            src="<?php echo $img_flc; ?>"
-            />
+        <a href="http://facebook.com/seacidadefossenossa" target="_blank"
+            title="Facebook">
+          <img alt="Facebook" class="footer-social-icon"
+              src="<?php echo $icn_facebook; ?>" />
         </a>
       </li>
       <li>
-        <a href="https://facebook.com/psolcarioca/" title="PSOL Carioca">
-          <img
-            alt="PSOL Carioca"
-            class="footer-supporter-banner"
-            src="<?php echo $img_psol; ?>"
-            />
+        <a href="mailto:seacidadefossenossa@gmail.com" target="_blank"
+            title="E-mail">
+          <img alt="E-mail" class="footer-social-icon"
+              src="<?php echo $icn_email; ?>" />
+        </a>
+      </li>
+    </ul>
+    <ul class="footer-list footer-supporters">
+      <li>
+        <a href="http://laurocampos.org.br/" target="_blank"
+            title="Fundação Lauro Campos">
+          <img alt="Fundação Lauro Campos" class="footer-supporter-banner"
+              src="<?php echo $img_flc; ?>" />
+        </a>
+      </li>
+      <li>
+        <a href="https://facebook.com/psolcarioca/" target="_blank"
+            title="PSOL Carioca">
+          <img alt="PSOL Carioca" class="footer-supporter-banner"
+              src="<?php echo $img_psol; ?>" />
         </a>
       </li>
     </ul>
