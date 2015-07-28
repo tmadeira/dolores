e3d05a7d477396ad43f2939a71242a03fe61dd3f
Tiago Madeira <tmadeira@gmail.com>
2015-07-27 18:31:46 -0700

    Add share buttons to single and page footer

--- src/php/single.php
+++ src/php/single.php
@@ -5,7 +5,7 @@ get_header();
 <main class="single wrap">
   <article class="single-content">
     <h2 class="single-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <div class="single-meta">
+    <div class="single-meta social-media">
       <span class="single-date">
         <?php the_time('d \d\e F \d\e Y'); ?>
       </span>
@@ -15,6 +15,11 @@ get_header();
 
     <div class="entry">
       <?php the_content(); ?>
+    </div>
+
+    <div class="single-meta social-media">
+      <div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="recommend" data-show-faces="false" data-share="false"></div>
+      <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-lang="pt"></a>
     </div>
   </article>
 </main>
