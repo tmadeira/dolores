64614909129f9d8182defea82c99866acff66957
Tiago Madeira <tmadeira@gmail.com>
2015-09-29 17:21:03 -0300

    Change button to link in taxonomy-tema.php

--- src/php/taxonomy-tema.php
+++ src/php/taxonomy-tema.php
@@ -11,7 +11,7 @@ get_header();
     <div class="tema-info">
       <h2 class="tema-name"><?php single_cat_title(); ?></h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <button class="tema-button-more">Ver diagnóstico</button>
+      <a class="tema-link-more" href="#">Ver diagnóstico</a>
     </div>
   </div>
 </main>
