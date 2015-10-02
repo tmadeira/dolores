571ea6597bf6c046a37f9e7f0c50868ab3199123
Tiago Madeira <tmadeira@gmail.com>
2015-10-02 18:34:07 -0300

    Fix author permalink

--- src/php/dlib/wp_util/setup_permalinks.php
+++ src/php/dlib/wp_util/setup_permalinks.php
@@ -1,15 +1,17 @@
 <?php
 class DoloresPermalinks {
-  public static function flush_rules() {
+  public function __construct() {
     global $wp_rewrite;
     $wp_rewrite->author_base = 'perfil';
+    add_action('after_switch_theme', Array($this, 'flush_rules'));
+    // Uncomment the line below to force flush
+    //add_action('wp_loaded', Array($this, 'flush_rules'));
+  }
+
+  public function flush_rules() {
+    global $wp_rewrite;
     $wp_rewrite->flush_rules(false);
   }
 };
 
-add_action('after_switch_theme', Array('DoloresPermalinks', 'flush_rules'));
-
-/**
- * Uncomment the line below to force flush
- */
-//add_action('wp_loaded', Array('DoloresPermalinks', 'flush_rules'));
+new DoloresPermalinks();
