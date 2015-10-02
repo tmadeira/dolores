c5ba3cf876a480055ffd677a15718ad85dd8997b
Tiago Madeira <tmadeira@gmail.com>
2015-10-02 16:09:05 -0300

    Start adding sections to home

--- src/php/v2_index.php
+++ src/php/v2_index.php
@@ -57,6 +57,60 @@ if (!$paged || $paged == 1) {
     </section>
     <?php
   }
+
+  ?>
+
+  <section class="home-default-section">
+    <div class="wrap">
+      <ul class="home-main-grid">
+        <li class="home-main-item home-bg-map">
+          <a href="#" class="home-main-item-link home-main-item-link-no-alpha">
+            <div class="home-main-item-border"></div>
+            <div class="home-main-item-wrap">
+              <h3 class="home-main-item-title">
+                Que mudanças você quer para o seu bairro?
+              </h3>
+              <button class="home-main-item-action">Participe</button>
+            </div>
+          </a>
+        </li>
+
+        <?php
+        $image = "http://imguol.com/c/noticias/2013/10/23/7-lugar-houston-eua---houston-no-estado-do-texas-ficou-em-7-lugar-esta-e-a-quarta-maior-cidade-dos-estados-unidos-quando-se-trata-de-populacao-mas-a-atracao-com-certeza-nao-e-paisagem-1382537404960_956x500.jpg";
+        ?>
+        <li
+            class="home-main-item"
+            style="background-image: url('<?php echo $image; ?>');"
+            >
+          <a href="#" class="home-main-item-link">
+            <div class="home-main-item-wrap">
+              <h3 class="home-main-item-title">
+                Viver na cidade
+              </h3>
+              <p class="home-main-item-explanation">
+                E se as decisões fossem nossas?
+              </p>
+              <button class="home-main-item-action">Participe</button>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="home-ideas">
+    <div class="wrap">
+      <h2 class="home-title">Ideias mais comentadas</h2>
+
+      <!-- TODO: grid -->
+
+      <div class="home-button-container">
+        <a class="home-button" href="/temas">Veja todos os temas</a>
+      </div>
+    </div>
+  </section>
+
+  <?php
 }
 
 include(__DIR__ . '/grid.php');
