4d5441eff75892bd3e47ef5eb6b6195aad0c98c6
Tiago Madeira <tmadeira@gmail.com>
2015-10-06 11:01:27 -0300

    Use Dolores profile pictures in /wp-admin/users.php

---
+++ src/php/dlib/wp_util/setup_avatar.php
@@ -0,0 +1,28 @@
+<?php
+function dolores_get_avatar($avatar, $id_or_email, $size, $default, $alt) {
+	$user = false;
+
+	if (is_numeric($id_or_email)) {
+		$id = (int) $id_or_email;
+		$user = get_user_by('id' , $id);
+	} else if (is_object($id_or_email)) {
+		if (!empty($id_or_email->user_id)) {
+			$id = (int) $id_or_email->user_id;
+			$user = get_user_by('id', $id);
+		}
+	} else {
+		$user = get_user_by('email', $id_or_email);
+	}
+
+	if ($user && is_object($user)) {
+    require_once(__DIR__ . '/user_meta.php');
+    $url = dolores_get_profile_picture($user);
+    $avatar = "<img alt='{$alt}' src='{$url}' ";
+    $avatar.= "  class='avatar avatar-{$size} photo' ";
+    $avatar.= "  height='{$size}' width='{$size}' />";
+	}
+
+  return $avatar;
+}
+
+add_filter('get_avatar', 'dolores_get_avatar', 1, 5);
--- src/php/functions.php
+++ src/php/functions.php
@@ -4,6 +4,7 @@ require_once(__DIR__ . '/dlib/wp_util/disable_emojis.php');
 require_once(__DIR__ . '/dlib/wp_util/modify_queries.php');
 require_once(__DIR__ . '/dlib/wp_util/register_menus.php');
 require_once(__DIR__ . '/dlib/wp_util/register_post_types.php');
+require_once(__DIR__ . '/dlib/wp_util/setup_avatar.php');
 require_once(__DIR__ . '/dlib/wp_util/setup_editor.php');
 require_once(__DIR__ . '/dlib/wp_util/setup_opengraph.php');
 require_once(__DIR__ . '/dlib/wp_util/setup_permalinks.php');
