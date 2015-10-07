78e4596e06aef1493005ffde6135f53e9f53dc1f
Tiago Madeira <tmadeira@gmail.com>
2015-10-07 18:42:53 -0300

    Show 9 posts in author and taxonomy templates

--- src/php/dlib/wp_util/modify_queries.php
+++ src/php/dlib/wp_util/modify_queries.php
@@ -3,6 +3,10 @@ function dolores_pre_get_posts($query) {
   if ($query->is_author && $query->is_main_query()) {
     $query->query_vars['post_type'] = 'ideia';
   }
+
+  if ($query->is_author || $query->is_tax) {
+    $query->set('posts_per_page', 9);
+  }
 }
 
 add_action('pre_get_posts', 'dolores_pre_get_posts');
