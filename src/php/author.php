a777d7ccccf5017cae00f12334bb46caa2a6329e
Tiago Madeira <tmadeira@gmail.com>
2015-10-15 00:49:11 -0300

    Show less information in paged templates

--- src/php/author.php
+++ src/php/author.php
@@ -6,35 +6,37 @@ $info = isset($_GET['author_name']) ? get_user_by('slug', $author_name) :
 require_once(__DIR__ . '/dlib/wp_util/user_meta.php');
 $picture = dolores_get_profile_picture($info);
 $pic_style = ' style="background-image: url(\'' . $picture. '\');"';
-?>
 
-<main class="profile">
-  <div class="wrap">
-    <div class="profile-picture"<?php echo $pic_style; ?>>
+if (!$paged || $paged == 1) {
+  ?>
+  <main class="profile">
+    <div class="wrap">
+      <div class="profile-picture"<?php echo $pic_style; ?>>
+      </div>
+
+      <div class="profile-info">
+        <h2 class="profile-name"><?php echo $info->display_name; ?></h2>
+        <p class="profile-stats">
+          <?php echo $wp_query->found_posts; ?> ideias
+          &bullet;
+          <?php echo dolores_get_comment_count_for_user($info); ?> respostas
+        </p>
+
+        <h3 class="profile-data-title">Informações pessoais</h3>
+        <!-- TODO -->
+        <ul class="profile-data">
+          <li><strong>Aniversário</strong> 8 de janeiro</li>
+          <li><strong>Instituição de ensino</strong> UFF</li>
+          <li><strong>Curso</strong> História</li>
+        </dl>
+      </div>
     </div>
+  </main>
 
-    <div class="profile-info">
-      <h2 class="profile-name"><?php echo $info->display_name; ?></h2>
-      <p class="profile-stats">
-        <?php echo $wp_query->found_posts; ?> ideias
-        &bullet;
-        <?php echo dolores_get_comment_count_for_user($info); ?> respostas
-      </p>
+  <h2 class="author-grid-title">Ideias de <?php echo $info->display_name; ?></h3>
+  <?php
+}
 
-      <h3 class="profile-data-title">Informações pessoais</h3>
-      <!-- TODO -->
-      <ul class="profile-data">
-        <li><strong>Aniversário</strong> 8 de janeiro</li>
-        <li><strong>Instituição de ensino</strong> UFF</li>
-        <li><strong>Curso</strong> História</li>
-      </dl>
-    </div>
-  </div>
-</main>
-
-<h2 class="author-grid-title">Ideias de <?php echo $info->display_name; ?></h3>
-
-<?php
 require_once(__DIR__ . '/grid-ideias.php');
 dolores_grid_ideias();
 ?>
--- src/php/taxonomy-tema.php
+++ src/php/taxonomy-tema.php
@@ -1,125 +1,129 @@
 <?php
 // TODO: use get_term_meta($term->term_id, 'image') as OpenGraph image
 get_header();
-$term = get_queried_object();
 
-$video = get_term_meta($term->term_id, 'video', true);
-$image = get_term_meta($term->term_id, 'image', true);
-$more = get_term_meta($term->term_id, 'more', true);
+if (!$paged || $paged == 1) {
+  $term = get_queried_object();
 
-$vparams = "rel=0&amp;controls=0&amp;showinfo=0";
-?>
+  $video = get_term_meta($term->term_id, 'video', true);
+  $image = get_term_meta($term->term_id, 'image', true);
+  $more = get_term_meta($term->term_id, 'more', true);
 
-<main class="tema">
-  <div class="wrap">
-    <?php if ($video) { ?>
-      <div class="tema-video">
-        <iframe
-          allowfullscreen
-          frameborder="0"
-          height="480"
-          src="https://youtube.com/embed/<?php echo $video . "?" . $vparams; ?>"
-          width="640"
-          >
-        </iframe>
-      </div>
-    <?php } else if ($image) {
-      $style = ' style="background-image: url(\'' . $image . '\');"';
-      ?>
-      <div class="tema-video">
-        <div class="tema-video-image"<?php echo $style;?>> </div>
-      </div>
-    <?php } ?>
-
-    <div class="tema-info">
-      <h2 class="tema-name">
-        <?php
-        if ($term->parent != 0) {
-          echo "#";
-        }
-        single_cat_title();
-        ?>
-      </h2>
-      <p><?php echo category_description(); ?></p>
-      <?php if ($more) { ?>
-        <a class="tema-link-more" href="<?php echo $more; ?>">
-          Ver diagnóstico
-        </a>
-      <?php } ?>
-    </div>
-  </div>
-</main>
-
-<?php
-if ($term->parent == 0) {
+  $vparams = "rel=0&amp;controls=0&amp;showinfo=0";
   ?>
-  <section class="tema-form bg-pattern-orange">
-    <div class="wrap tema-form">
-      <div class="tema-form-explanation">
-        <h2 class="tema-form-title">
-          Se a<br />cidade<br />fosse<br />nossa?
-        </h2>
-        <p class="tema-form-description">
-          Você tem alguma ideia para o tema
-          <strong><?php single_cat_title(); ?></strong>?
-        </p>
-        <p class="tema-form-description">
-          Compartilhe usando o formulário!
-        </p>
-      </div>
 
-      <form class="tema-form-form" id="form-tema">
-        <p class="tema-form-item">
-          <label class="tema-form-label" for="tema-form-title">
-            Título
-          </label>
-          <input
-              type="text"
-              name="title"
-              class="tema-form-input"
-              id="tema-form-title"
-              maxlength="100"
-              placeholder="Título (max. 100 caracteres)"
-              />
-        </p>
-        <p class="tema-form-item">
-          <label class="tema-form-label" for="tema-form-content">
-            Escreva sua ideia
-          </label>
-          <textarea
-              class="tema-form-textarea"
-              name="text"
-              id="tema-form-content"
-              maxlength="600"
-              placeholder="Ideia (max. 600 caracteres)"
-              ></textarea>
-        </p>
-        <p class="tema-form-item">
-          <label class="tema-form-label" for="tema-form-tags">
-            Tags
-          </label>
-          <input
-              type="text"
-              class="tema-form-input"
-              name="tags"
-              id="tema-form-tags"
-              placeholder="Escolha algumas palavras-chave"
-              />
-        </p>
-        <p class="tema-form-item" style="margin-top: 5px; text-align: right;">
-          <input type="hidden" name="cat" value="<?php echo $term->slug; ?>" />
-          <button class="tema-form-button" type="submit">
-            Enviar
-          </button>
-        </p>
-      </form>
+  <main class="tema">
+    <div class="wrap">
+      <?php if ($video) { ?>
+        <div class="tema-video">
+          <iframe
+            allowfullscreen
+            frameborder="0"
+            height="480"
+            src="https://youtube.com/embed/<?php echo $video . "?" . $vparams; ?>"
+            width="640"
+            >
+          </iframe>
+        </div>
+      <?php } else if ($image) {
+        $style = ' style="background-image: url(\'' . $image . '\');"';
+        ?>
+        <div class="tema-video">
+          <div class="tema-video-image"<?php echo $style;?>> </div>
+        </div>
+      <?php } ?>
+
+      <div class="tema-info">
+        <h2 class="tema-name">
+          <?php
+          if ($term->parent != 0) {
+            echo "#";
+          }
+          single_cat_title();
+          ?>
+        </h2>
+        <p><?php echo category_description(); ?></p>
+        <?php if ($more) { ?>
+          <a class="tema-link-more" href="<?php echo $more; ?>">
+            Ver diagnóstico
+          </a>
+        <?php } ?>
+      </div>
     </div>
-  </section>
+  </main>
+
+  <?php
+  if ($term->parent == 0) {
+    ?>
+    <section class="tema-form bg-pattern-orange">
+      <div class="wrap tema-form">
+        <div class="tema-form-explanation">
+          <h2 class="tema-form-title">
+            Se a<br />cidade<br />fosse<br />nossa?
+          </h2>
+          <p class="tema-form-description">
+            Você tem alguma ideia para o tema
+            <strong><?php single_cat_title(); ?></strong>?
+          </p>
+          <p class="tema-form-description">
+            Compartilhe usando o formulário!
+          </p>
+        </div>
+
+        <form class="tema-form-form" id="form-tema">
+          <p class="tema-form-item">
+            <label class="tema-form-label" for="tema-form-title">
+              Título
+            </label>
+            <input
+                type="text"
+                name="title"
+                class="tema-form-input"
+                id="tema-form-title"
+                maxlength="100"
+                placeholder="Título (max. 100 caracteres)"
+                />
+          </p>
+          <p class="tema-form-item">
+            <label class="tema-form-label" for="tema-form-content">
+              Escreva sua ideia
+            </label>
+            <textarea
+                class="tema-form-textarea"
+                name="text"
+                id="tema-form-content"
+                maxlength="600"
+                placeholder="Ideia (max. 600 caracteres)"
+                ></textarea>
+          </p>
+          <p class="tema-form-item">
+            <label class="tema-form-label" for="tema-form-tags">
+              Tags
+            </label>
+            <input
+                type="text"
+                class="tema-form-input"
+                name="tags"
+                id="tema-form-tags"
+                placeholder="Escolha algumas palavras-chave"
+                />
+          </p>
+          <p class="tema-form-item" style="margin-top: 5px; text-align: right;">
+            <input type="hidden" name="cat" value="<?php echo $term->slug; ?>" />
+            <button class="tema-form-button" type="submit">
+              Enviar
+            </button>
+          </p>
+        </form>
+      </div>
+    </section>
+    <?php
+  }
+  ?>
+
   <?php
 }
-?>
 
-<?php
 require_once(__DIR__ . '/grid-ideias.php');
 dolores_grid_ideias();
 ?>
