38c727e2f02c2879e6d4d5a3e2fd8a49bdea2fc2
Tiago Madeira <tmadeira@gmail.com>
2015-10-19 13:49:53 -0200

    Release v2

--- src/php/functions.php
+++ src/php/functions.php
@@ -15,6 +15,8 @@ require_once(__DIR__ . '/dlib/wp_admin/taxonomy.php');
 require_once(__DIR__ . '/dlib/wp_admin/users.php');
 
 function dolores_get_version() {
+  return 2;
+
   if (array_key_exists('v', $_GET)) {
     return $_GET['v'];
   }
