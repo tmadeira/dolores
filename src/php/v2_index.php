f9067699da23b98ee9afd4890f7f1c00624fe06e
Tiago Madeira <tmadeira@gmail.com>
2015-10-19 12:35:50 -0200

    Change video in homepage

--- src/php/v2_index.php
+++ src/php/v2_index.php
@@ -28,7 +28,7 @@ if (!$paged || $paged == 1) {
       <iframe
         width="853"
         height="480"
-        src="https://www.youtube.com/embed/5Zbs3grhys0?rel=0&amp;showinfo=0"
+        src="https://www.youtube.com/embed/XTXlM-F4zpQ?rel=0&amp;showinfo=0"
         frameborder="0"
         allowfullscreen
         >
