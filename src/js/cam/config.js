13134d53a73404f3e66dfd8b94ac3820974875fb
Tiago Madeira <tmadeira@gmail.com>
2016-01-31 11:09:54 -0200

    cam: Add JS boilerplate

---
+++ src/js/cam/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // This must match $break-* definitions in css/cam/_basic.scss
+  breakpoint: {
+    tablet: 850,
+    desktop: 1150,
+    bigDesktop: 1400
+  },
+
+  apiBaseURL: "/api/"
+
+  /*
+  ganalyticsUA: "UA-65584348-1",
+  facebookAppID: "1053667657991384",
+  googleAppId: "1047432988645-m9vje9cnt4p3p352hh9cokdo8h77dvm5" +
+               ".apps.googleusercontent.com",
+  gmapsKey: "AIzaSyBfx8GXbX53YasBdY1rL36OcK98ykwckes"
+  */
+};
