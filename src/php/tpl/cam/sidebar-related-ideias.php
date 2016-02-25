6ffc88db634d076897b43127e02e27ea971ad0b1
Tiago Madeira <tmadeira@gmail.com>
2016-02-25 20:54:35 -0300

    cam: Leave sidebar-related-ideias.php blank

--- src/php/tpl/cam/sidebar-related-ideias.php
+++ src/php/tpl/cam/sidebar-related-ideias.php
@@ -1,8 +1,11 @@
+<?php
+/*
+TODO
 <div class="sidebar-section">
   <h2 class="sidebar-title">
     <span class="bg-red">Propostas relacionadas</span>
   </h2>
 
   <p>Em construção.</p>
-  <!-- TODO -->
 </div>
+*/
