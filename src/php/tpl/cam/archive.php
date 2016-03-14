6f850dd9f4d2346885e1d4c2110216c938828fdf
Tiago Madeira <tmadeira@gmail.com>
2016-03-14 16:51:30 -0300

    cam: Add archive description to template

--- src/php/tpl/cam/archive.php
+++ src/php/tpl/cam/archive.php
@@ -1,7 +1,10 @@
 <?php
 add_filter('get_the_archive_title', function($title) {
   if (is_category()) {
-    $title = single_cat_title();
+    $title = single_cat_title('', false);
+  }
+  if ($title == 'Contribuições') {
+    $title = 'Contribuições para o debate';
   }
   return $title;
 });
@@ -11,6 +14,15 @@ get_header();
 
 <main class="wrap default-wrap no-padding-bottom">
   <h2 class="archive-title"><?php the_archive_title(); ?></h2>
+  <?php
+  if (is_category() && category_description()) {
+    ?>
+    <div class="archive-description">
+      <?php echo category_description(); ?>
+    </div>
+    <?php
+  }
+  ?>
 </main>
 
 <?php
