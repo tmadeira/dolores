1504718a6d0eb663e56c5dbeb7f5a4447964288a
Tiago Madeira <tmadeira@gmail.com>
2016-03-17 14:31:36 -0300

    Display 404 instead of blank page when file does not exist in require.php

--- src/php/require.php
+++ src/php/require.php
@@ -1,2 +1,7 @@
 <?php
-require(DOLORES_TEMPLATE_PATH . '/' . basename(__FILE__));
+if (file_exists(DOLORES_TEMPLATE_PATH . '/' . basename(__FILE__))) {
+  require(DOLORES_TEMPLATE_PATH . '/' . basename(__FILE__));
+} else {
+  Header('HTTP/1.1 404 Not Found');
+  require(DOLORES_PATH . '/404.php');
+}
