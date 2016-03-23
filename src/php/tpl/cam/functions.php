f0e77f93cef7502ecd1969e1889d1691684c8e53
Tiago Madeira <tmadeira@gmail.com>
2016-03-23 15:14:00 -0300

    cam: Do not allow contributors to edit profile

--- src/php/tpl/cam/functions.php
+++ src/php/tpl/cam/functions.php
@@ -8,6 +8,15 @@ require_once(DOLORES_TEMPLATE_PATH . '/grid-ideias.php');
 
 define('CALENDAR_ID', '4spn2cn6gpde1jf0mpdu34ti7s@group.calendar.google.com');
 
+function dolores_remove_profile() {
+	remove_menu_page('profile.php');
+	remove_submenu_page('users.php', 'profile.php');
+	if (IS_PROFILE_PAGE === true) {
+		wp_die('Sem permissões suficientes.');
+	}
+}
+add_action('admin_menu', 'dolores_remove_profile');
+
 function dolores_random_orderby() {
   return "RAND()";
 }
